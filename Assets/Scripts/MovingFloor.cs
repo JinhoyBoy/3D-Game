2d19fa93d5f601f6c8fcc67ae7632b673f3d6cb3
Jinho An <jinhoan0422@gmail.com>
2024-11-30 15:05:06 +0900

    level1 update again

--- Assets/Scripts/MovingFloor.cs
+++ Assets/Scripts/MovingFloor.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class MovingFloor : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float rotationspeed = 45f;
+    public float rotationSpeed = 45f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0f,0f,rotationspeed * Time.deltaTime);
+        transform.Rotate(0f,0f,rotationSpeed * Time.deltaTime);
     }
 }
